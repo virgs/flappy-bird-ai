201eaeb6c16ac1820e71e7ceb066c524824983e6
Guilherme Moraes <guilherme.moraes@ifood.com.br>
2021-05-04 14:19:08 -0300

    Adjust html tags values

--- src/actors/bird.ts
+++ src/actors/bird.ts
@@ -25,7 +25,6 @@ export class Bird {
         this.registerEvents(options.scene);
     }
 
-
     private createSprite(options: { initialPosition: Phaser.Geom.Point; scene: Phaser.Scene }): Phaser.GameObjects.Sprite {
         const birdSprite = options.scene.add.sprite(options.initialPosition.x, options.initialPosition.y, this.birdTextureKey);
         birdSprite.setScale(scale * this.assetsProportion);
@@ -52,7 +51,6 @@ export class Bird {
 
     private handlePipeCollision(options) {
         if (options.sprites.some(pipeSprite => RectangleToRectangle(pipeSprite.getBounds(), this.birdSprite.getBounds()))) {
-            this.birdSprite.setAngle(45);
             this.alive = false;
             this.birdSprite.setAlpha(0.4);
             this.birdSprite.anims.stop();
@@ -62,18 +60,12 @@ export class Bird {
     public update(options: { delta, pixelsPerSecond }): void {
         if (this.alive) {
             this.handleBirdInput(options.delta);
-            const birdBounds = this.birdSprite.getBounds();
-            if (birdBounds.top <= 0 || birdBounds.bottom > dimensionHeight * scale) {
+            if (this.birdIsOutOfBounds()) {
                 this.alive = false;
                 this.birdSprite.setAlpha(0.4);
                 this.birdSprite.anims.stop();
             } else {
-                const instantVerticalVelocity = this.gravity * options.delta;
-                this.birdSprite.y = this.birdSprite.y + options.delta * (this.verticalSpeed + instantVerticalVelocity / 2);
-                this.verticalSpeed = this.verticalSpeed + instantVerticalVelocity;
-                if (this.verticalSpeed > this.maxVerticalSpeed) {
-                    this.verticalSpeed = this.maxVerticalSpeed;
-                }
+                this.applyGravity(options);
                 this.birdSprite.setAngle((this.verticalSpeed / this.maxVerticalSpeed) * 45);
             }
         } else {
@@ -81,6 +73,15 @@ export class Bird {
             if (this.birdSprite.x + this.birdSprite.displayWidth < 0) {
                 this.birdSprite.destroy();
             }
+        }
+    }
+
+    private applyGravity(options: { delta; pixelsPerSecond }) {
+        const instantVerticalVelocity = this.gravity * options.delta;
+        this.birdSprite.y = this.birdSprite.y + options.delta * (this.verticalSpeed + instantVerticalVelocity / 2);
+        this.verticalSpeed = this.verticalSpeed + instantVerticalVelocity;
+        if (this.verticalSpeed > this.maxVerticalSpeed) {
+            this.verticalSpeed = this.maxVerticalSpeed;
         }
     }
 
@@ -95,4 +96,10 @@ export class Bird {
         }
 
     }
+
+    private birdIsOutOfBounds() {
+        const birdBounds = this.birdSprite.getBounds();
+        return birdBounds.top <= 0 || birdBounds.bottom > dimensionHeight * scale;
+    }
+
 }
